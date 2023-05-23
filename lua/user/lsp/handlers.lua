f18189b97718c4414435fc9e38b2489f73f4ce02
ihtesham <ihteshamulhaq510@gmail.com>
2023-05-23 07:56:34 +0500

    dashborad update

--- lua/user/lsp/handlers.lua
+++ lua/user/lsp/handlers.lua
@@ -73,11 +73,11 @@ end
 
 M.on_attach = function(client, bufnr)
 	if client.name == "tsserver" then
-		client.server_capabilities.documentFormattingProvider = false
+		client.server_capabilities.documentFormattingProvider = true
 	end
 
 	if client.name == "sumneko_lua" then
-		client.server_capabilities.documentFormattingProvider = false
+		client.server_capabilities.documentFormattingProvider = true
 	end
 
 	lsp_keymaps(bufnr)
