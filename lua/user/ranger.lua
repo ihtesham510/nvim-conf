b253bf5f5f5a66d01275afd2d17ae148dc313279
ihtesham <ihteshamulhaq510@gmail.com>
2023-06-18 16:36:55 +0500

    added ranger auto run on start

--- lua/user/ranger.lua
+++ lua/user/ranger.lua
@@ -1,7 +1,14 @@
 require("ranger-nvim").setup({ replace_netrw = true })
-vim.api.nvim_set_keymap("n", "sf", "", {
+vim.api.nvim_set_keymap("n", "<leader>r", "", {
 	noremap = true,
 	callback = function()
 		require("ranger-nvim").open(true)
 	end,
 })
+
+vim.cmd([[
+  augroup MyAutoCmds
+    autocmd!
+    autocmd VimEnter * :RnvimrStartBackground
+  augroup END
+]])
